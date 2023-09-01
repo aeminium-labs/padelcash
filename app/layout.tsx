84151d75c453dc90de1ea669ba0c0f5b50043288
Bernardo Raposo <hi@bernardoraposo.com>
2023-09-01 15:22:02 +0100

    Uses native script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import { Poppins as FontSans } from "next/font/google";
 
 import "@/styles/globals.css";
@@ -26,10 +25,10 @@ export default function RootLayout({ children }: Props) {
     return (
         <html lang="en" className={`${fontSans.variable} dark`}>
             <head>
-                <Script
-                    strategy="beforeInteractive"
+                <script
+                    defer
                     src="https://progressier.app/c00Kg9Z1JBplPIq0ICqp/script.js"
-                ></Script>
+                ></script>
             </head>
             <body className="min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
                 <Providers>
